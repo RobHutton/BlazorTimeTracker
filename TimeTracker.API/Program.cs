ddf6961e0736cd3809c41246a70524e3c0b3deba
Robyn Cerda <robyn_hutton@yahoo.com>
2025-04-06 11:32:16 -0500

    Add Blazor WebAssembly Standalone App Modify API Layer to Support WebAssemblyClient

--- TimeTracker.API/Program.cs
+++ TimeTracker.API/Program.cs
@@ -3,7 +3,8 @@ using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddLogging();
 builder.Services.AddOpenApi();  // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddDbContext<DataContext>(options =>
@@ -21,12 +22,19 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.MapScalarApiReference();
     app.MapOpenApi();
+    app.UseWebAssemblyDebugging();
 }
 
 app.UseHttpsRedirection();
 
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
